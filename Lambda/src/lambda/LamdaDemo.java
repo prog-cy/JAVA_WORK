84eb57da6d659a8005c968833365017bbce2c687
prog-cy <rupeshdwivedi3945@gmail.com>
2022-06-17 17:09:54 +0530

    modified

--- Lambda/src/lambda/LamdaDemo.java
+++ Lambda/src/lambda/LamdaDemo.java
@@ -1,5 +1,23 @@
 package lambda;
 
-public class LamdaDemo {
+// Functional interface: Functional interface is a special interface which only 
+//contains one abstract method.
 
+// Lambda Expression: A lambda expression is an anonymous method which provides implementation of the functional
+// interface's  abstract method.
+interface FuncInterface{
+	
+	//abstract method
+	void show();
+}
+
+//Main class
+public class LamdaDemo {
+	
+	public static void main(String[] args) {
+		
+		FuncInterface fobj = ()->System.out.println("I am learning Lambda Expression.");
+		
+		fobj.show(); // Calling abstract method
+	}
 }
